112cf699703f1b6037177d7299773f40dde81325
haris-75 <haris.ejaz@datumbrain.com>
2023-02-18 00:40:54 +0500

    Add Scripts Folder & Set Up Pages For  App

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,14 @@
-import "../styles/globals.css";
+import WebApp from '../layout/WebApp';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <WebApp>
+        <Component {...pageProps} />
+      </WebApp>
+    </div>
+  );
 }
 
 export default MyApp;
